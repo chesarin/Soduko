04c5c2348ab3a6c8208a7f132f034175d13caa07
chesarin <vargasces@gmail.com>
2010-11-12 13:29:50 -0500

    Makefile added so files can work with gmake and make

--- SodukoBoard.h
+++ SodukoBoard.h
@@ -1,9 +1,15 @@
 #ifndef SODUKOBOARD_H_
 #define SODUKOBOARD_H_
+#define MAX 10
+#include <iostream>
+#include <sstream>
+using namespace std;
 class SodukoBoard{
  private:
-  int board[10][10]; 
+  int board[MAX][MAX]; 
  public:
-
+  SodukoBoard();
+  void fill_up(string line,int line_num);
+  void print_board();
 };
-#endf
+#endif
